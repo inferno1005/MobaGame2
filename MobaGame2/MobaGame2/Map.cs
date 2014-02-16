32ca2703b9f4bd6f5a0e9538f3cfb9be477e174e
inferno1005 <inferno1005@gmail.com>
2014-02-15 22:46:24 -0600

    changed map size

--- MobaGame2/MobaGame2/Map.cs
+++ MobaGame2/MobaGame2/Map.cs
@@ -21,7 +21,7 @@ namespace MobaGame2
             this.position = new Vector2(30, 30);
             this.attribute.clickable = true;
             this.height = 800;
-            this.width = 1300;
+            this.width = 5000;
             texturename = "texture\\seamless_ground";
         }
     }
